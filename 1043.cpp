8be7815fe01185326e9667cf1c079e0918dc2e53
confusedman <confusedman0@gmail.com>
2015-08-31 18:25:47 +0600

    Update 1043.cpp
    I think it's more efficient way...

--- 1043.cpp
+++ 1043.cpp
@@ -1,33 +1,17 @@
 #include <iostream>
-#include <stdio.h>
+#include <iomanip>
 
 using namespace std;
 
-int main(){
-    float a,b,c;
-    float mod1, mod2, mod3;
-    
+int main()
+{
+    float a, b, c;
     cin >> a >> b >> c;
-    
-    if(b - c < 0)
-         mod1 = c - b;
-    else
-        mod1 = b - c;
-        
-    if(b - a < 0)
-         mod2 = a - b;
-    else
-        mod2 = b - a;
-        
-    if(a - c < 0)
-         mod3 = c - a;
-    else
-        mod3 = a - c;
-    
-    if(mod1 < a && a < (b + c) && mod2 < c && c < (a+b) && mod3 < b && b < (a+c))
-            printf("Perimetro = %.1f\n", a+b+c);
-    else
-            printf("Area = %.1f\n", ((a+b)*c)/2.0);
-            
-    return 0;   
+    if((b+c > a) && (a+b > c) && (c+a) > b){
+        cout << fixed << setprecision(1) << "Perimetro = " << (a + b + c) << endl;
+    }
+    else{
+        cout << fixed << setprecision(1) << "Area = " << 0.5*(a + b)*c << endl;
+    }
+    return 0;
 }
